4134355196fca2575ab4383fa8fe185cdccaf97e
Len Mahaffey <len.mahaffey@gmail.com>
2018-03-28 20:48:54 -0600

    displayWindow now has a black background

--- OSXDualScreen/displayViewController.swift
+++ OSXDualScreen/displayViewController.swift
@@ -13,6 +13,9 @@ class displayViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        self.view.wantsLayer = true
+        let color : CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        self.view.layer?.backgroundColor = color
     }
     
 }
