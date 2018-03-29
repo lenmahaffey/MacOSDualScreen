583437f48b29085264cc88f1e5cb33c7cb9e79fc
Len Mahaffey <len.mahaffey@gmail.com>
2018-03-28 20:50:15 -0600

    added NSScreen var and if loop to look for second display.

--- OSXDualScreen/displayWindowController.swift
+++ OSXDualScreen/displayWindowController.swift
@@ -12,8 +12,12 @@ class displayWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-        self.showWindow(nil)
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        let screens = NSScreen.screens
+        if screens.count == 1 {
+            //self.window?.toggleFullScreen(nil)
+            self.showWindow(nil)
+        }
     }
 
 }
