3e61a361fd8ea40385286505c36a2d0ab8dc441b
Len Mahaffey <len.mahaffey@gmail.com>
2018-04-01 15:38:00 -0600

    added a computed var to hold displayWindowController and added a notification observer and callback for monitor events and removed unused boilerplate code

--- OSXDualScreen/ViewController.swift
+++ OSXDualScreen/ViewController.swift
@@ -10,6 +10,24 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    var displayWindowController: displayWindowController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        let storyboardSceneID = NSStoryboard.SceneIdentifier(rawValue: "displayWindowController")
+        let wc = storyboard.instantiateController(withIdentifier: storyboardSceneID) as! displayWindowController
+        return wc
+    }
+    
+    required init?(coder: NSCoder){
+        super.init(coder: coder)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(monitorDidChange),
+            name:  NSApplication.didChangeScreenParametersNotification,
+            object: nil)
+    }
+    
+    override func viewWillAppear() {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +38,12 @@ class ViewController: NSViewController {
         _ = storyboard.instantiateController(withIdentifier: storyboardSceneID) as! NSWindowController
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @objc func monitorDidChange(notification: NSNotification) {
+        if NSScreen.screens.count == 1 {
+            print("there is one monitor")
+        }
+        if NSScreen.screens.count > 1 {
+            print("there are two monitors")
         }
     }
 }
