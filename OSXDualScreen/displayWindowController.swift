36a1188c32b0b77a5bd6bebbb8ba14196d8f18ac
Len Mahaffey <len.mahaffey@gmail.com>
2018-04-01 15:35:59 -0600

    comment out fullscreen for troubleshooting

--- OSXDualScreen/displayWindowController.swift
+++ OSXDualScreen/displayWindowController.swift
@@ -22,7 +22,7 @@ class displayWindowController: NSWindowController {
         self.window?.setFrame(NSScreen.screens[1].frame, display: true)
         self.window?.toggleTabBar(self)
         self.window?.backgroundColor = NSColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
-        self.window?.toggleFullScreen(self)
+        //self.window?.toggleFullScreen(self)
         self.showWindow(self)
     }
 }
